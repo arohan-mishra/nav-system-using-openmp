b23fc1c8477bca531c1225c1eb361d7d7656b732
Arohan Mishra <56361104+arohan-mishra@users.noreply.github.com>
2021-12-10 21:37:31 +0530

    Update serial.cpp

--- serial.cpp
+++ serial.cpp
@@ -6,65 +6,68 @@
 using namespace std;
 using namespace std::chrono;
 
-#define V 3
+#define V 51 //define dimension of matix
 #define INFINITY 100000
 
-int minDistance(int dist[], bool sptSet[])
+//to find the minimum path from the current node
+int minPathLength(int length[], bool setPath[])
 {
 	int min = INFINITY, min_index;
 
 	for (int v = 0; v < V; v++)
-		if (sptSet[v] == false && dist[v] <= min)
-			min = dist[v], min_index = v;
+		if (setPath[v] == false && length[v] <= min)
+			min = length[v], min_index = v; //min length set to length of the node frm infinity
 
-	return min_index;
+	return min_index; //returns the index with the min path
 }
 
-void printSolution(int dist[], int n)
+//prints the solution
+void print(int length[], int n)
 {
 	printf("VERTEX \t\t DISTANCE FROM SOURCE\n");
 	for (int i = 0; i < V; i++)
     {
-        if(dist[i]==INFINITY)
+        if(length[i]==INFINITY)
             printf("%d \t\t INFINITY\n", i);
         else 
-            printf("%d \t\t %d\n", i, dist[i]);
+            printf("%d \t\t %d\n", i, length[i]);
     }
 }
 
-void dijkstra(int graph[V][V], int src)
+//the core code where the dijkstra algorithm is implemented
+//takes the graph and a value zero as input 
+void dijkstra_code(int map[V][V], int src)
 {
-	int dist[V]; 
+	int length[V]; //length of the node in the graph
 
-	bool sptSet[V]; 
+	bool setPath[V]; //if the node is in the shortes path it will be true
 
 	for (int i = 0; i < V; i++)
-		dist[i] = INFINITY, sptSet[i] = false;
-
-	dist[src] = 0;
+		length[i] = INFINITY, setPath[i] = false; //initiale node length is infinite and setPath value is false
+	length[src] = 0;
 
 	for (int count = 0; count < V - 1; count++) {
 
-		int u = minDistance(dist, sptSet);
+		int u = minPathLength(length, setPath); //sets the minimum path length
 
-		sptSet[u] = true;
+		setPath[u] = true;
 
 		for (int v = 0; v < V; v++)
-			if (!sptSet[v] && graph[u][v] && dist[u] != INFINITY
-				&& dist[u] + graph[u][v] < dist[v])
-				dist[v] = dist[u] + graph[u][v];
+			if (!setPath[v] && map[u][v] && length[u] != INFINITY
+				&& length[u] + map[u][v] < length[v])
+				length[v] = length[u] + map[u][v];
 	}
 
-	printSolution(dist, V);
+	print(length, V);
 }
 
 int main()
 {
-	FILE* city = fopen("city2.txt", "r");
+	FILE* city = fopen("city4.txt", "r"); //the iput graph is taken from a txt file
 
     char c;
-    int n, num = 0, i = 0, j = 0;
-    int graph[3][3];
+    int n, no = 0, i = 0, j = 0;
+    int map[51][51];
 
     while(!feof(city)) 
     {
@@ -72,21 +75,21 @@ int main()
 
         if(isdigit(c))
         {
-            num = num * 10 + int(c) - 48;
+            no = no * 10 + int(c) - 48;
         }
 
         else if(c == ' ')
         {
-            graph[i][j] = num;
-            num = 0;
+            map[i][j] = no;
+            no = 0;
             j++;
         }
 
         else if(int(c) == 10 || int(c) == -1)
         {
-            graph[i][j] = num;
+            map[i][j] = no;
             n = j;
-            num = 0;
+            no = 0;
             j = 0;
             i++;
         }
@@ -94,16 +97,16 @@ int main()
 
     fclose(city);
 
-    auto start = high_resolution_clock::now();
+    auto start = high_resolution_clock::now(); //the start method to calculate runtime 
 	
-	dijkstra(graph, 0);
+	dijkstra_code(map, 0); //implement the algorithm
 
-    auto stop = high_resolution_clock::now();
+    auto end = high_resolution_clock::now(); //end the run time
 
-    auto duration = duration_cast<microseconds>(stop - start);
+    auto time_duration = duration_cast<microseconds>(end - start); 
   
     cout << "Running time: "
-         << duration.count() << " ms\n";
+         << time_duration.count() << " ms\n";
 
 	return 0;
 }
